555b166cf2796e271b0e29c6763da0798388b36d
oskarvitko <oskarvitko16@gmail.com>
2023-06-11 15:13:09 +0200

    update time + build

--- src/components/countdown/index.ts
+++ src/components/countdown/index.ts
@@ -1,6 +1,6 @@
 import LightCountdown from './countdown.module'
 
 /* countdown */
-let dateEnd = new Date('2023-08-15T15:30:00')
+let dateEnd = new Date('2023-08-15T15:00:00')
 
 export default () => new LightCountdown('.countdown', dateEnd)
