6e4716b93ab03c83d621da8274f5520bf54e35d1
oskarvitko <oskarvitko16@gmail.com>
2023-06-12 10:17:15 +0200

    added new video + build

--- src/components/title/index.ts
+++ src/components/title/index.ts
@@ -4,14 +4,16 @@ export const initTitle = () => {
     )
     const yesLabel = document.querySelector('[for=yes]')
     const noLabel = document.querySelector('[for=no]')
+    const videoBtn = document.querySelector<HTMLAnchorElement>('.main__btn')
 
-    if (node && yesLabel && noLabel) {
+    if (node && yesLabel && noLabel && videoBtn) {
         const text = getText()
         node.innerHTML = `${text.dear}, мы рады сообщить ${text.short}, что мы женимся!<br>${text.lets} разделить этот счастливый и
         долгожданный для нас праздник.`
 
         yesLabel.textContent = text.single ? 'Буду' : 'Будем присутствовать'
         noLabel.textContent = text.single ? 'Не буду' : 'Будем отсутствовать'
+        videoBtn.href = text.video
     }
 }
 
@@ -19,6 +21,7 @@ const getText = (): {
     dear: string
     lets: string
     short: string
+    video: string
     single?: boolean
 } => {
     const pathname = window.location.pathname.replace(/\//g, '')
@@ -28,12 +31,14 @@ const getText = (): {
                 dear: 'Дорогие гости',
                 lets: 'Приглашаем Вашу семью',
                 short: 'вам',
+                video: './video/video-many.mp4',
             }
         case 'c': {
             return {
                 dear: 'Дорогие гости',
                 lets: 'Приглашаем Вас',
                 short: 'вам',
+                video: './video/video-many.mp4',
             }
         }
         case 'u': {
@@ -42,6 +47,7 @@ const getText = (): {
                 lets: 'Приглашаем тебя',
                 short: 'тебе',
                 single: true,
+                video: './video/video-alone.mp4',
             }
         }
         default:
@@ -49,6 +55,7 @@ const getText = (): {
                 dear: 'Дорогие гости',
                 lets: 'Приглашаем Вас',
                 short: 'вам',
+                video: './video/video-many.mp4',
             }
     }
 }
